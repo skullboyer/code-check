c9574a209e4a09d7d2975f9782db8b73500c9371
skull <skull.gu@gmail.com>
2022-03-10 17:49:11 +0800

    [1]: 增加类型转换是否使用c-style cast规则；   [2]：修改魔鬼数字检查条件，可以检查'=|=='后的十进制和十六进制魔鬼数字，包括预处理指令使用涉及的魔鬼数字；   [3]: 加入c-style cast万能检查选项；   [4]: 更新版本至v0.3；

--- lint.py
+++ lint.py
@@ -365,6 +365,12 @@ lint_comment_style=
 # default: -1
 lint_blank_line=
 
+# rule.22
+# Whether the type conversion using C-style cast (static_cast | const_cast | reinterpret_cast)
+#  0: indifferent, 1: required
+# default: 0
+lint_cstyle_cast=
+
 """
 
 # We categorize each error message we print.  Here are the categories.
@@ -1091,6 +1097,8 @@ class _CppLintState(object):
     self.lint_comment_style = 0
     # rule.21: forbidden leave multi blank line
     self.lint_blank_line = -1
+    # rule.22: c-style cast indifferent
+    self.lint_cstyle_cast = 0
 
   def SetOutputFormat(self, output_format):
     """Sets the output format for errors."""
@@ -1370,6 +1378,11 @@ def SetBlankLine(blank):
 def BlankLine():
   return _cpplint_state.lint_blank_line
 
+def SetCstyleCast(cast):
+  _cpplint_state.lint_cstyle_cast = cast
+
+def CstyleCast():
+  return _cpplint_state.lint_cstyle_cast
 
 class _FunctionState(object):
   """Tracks current function name and the number of lines in its body."""
@@ -4680,7 +4693,7 @@ def GetLineWidth(line):
 # @skull.
 def CheckDevilFigure(filename, cleansed_line, line, prev_line, linenum, error):
   if DevilNumber() == -1:
-    if Search(r' = [1-9]', cleansed_line):
+    if Search(r' (=|==)\s*[1-9|0x]', cleansed_line):
       if not Match(r'^\s+(/\*|//)', prev_line) and not Match(r'^.*(/\*|//)', line):
         error(filename, linenum, 'build/devil', 3, 'This is probably using devil figure')
 
@@ -5690,39 +5703,40 @@ def CheckCStyleCast(filename, clean_lines, linenum, cast_type, pattern, error):
     True if an error was emitted.
     False otherwise.
   """
-  line = clean_lines.elided[linenum]
-  match = Search(pattern, line)
-  if not match:
-    return False
+  if CstyleCast() == 1:
+    line = clean_lines.elided[linenum]
+    match = Search(pattern, line)
+    if not match:
+      return False
 
-  # Exclude lines with keywords that tend to look like casts
-  context = line[0:match.start(1) - 1]
-  if Match(r'.*\b(?:sizeof|alignof|alignas|[_A-Z][_A-Z0-9]*)\s*$', context):
-    return False
+    # Exclude lines with keywords that tend to look like casts
+    context = line[0:match.start(1) - 1]
+    if Match(r'.*\b(?:sizeof|alignof|alignas|[_A-Z][_A-Z0-9]*)\s*$', context):
+      return False
 
-  # Try expanding current context to see if we one level of
-  # parentheses inside a macro.
-  if linenum > 0:
-    for i in xrange(linenum - 1, max(0, linenum - 5), -1):
-      context = clean_lines.elided[i] + context
-  if Match(r'.*\b[_A-Z][_A-Z0-9]*\s*\((?:\([^()]*\)|[^()])*$', context):
-    return False
+    # Try expanding current context to see if we one level of
+    # parentheses inside a macro.
+    if linenum > 0:
+      for i in xrange(linenum - 1, max(0, linenum - 5), -1):
+        context = clean_lines.elided[i] + context
+    if Match(r'.*\b[_A-Z][_A-Z0-9]*\s*\((?:\([^()]*\)|[^()])*$', context):
+      return False
 
-  # operator++(int) and operator--(int)
-  if context.endswith(' operator++') or context.endswith(' operator--'):
-    return False
+    # operator++(int) and operator--(int)
+    if context.endswith(' operator++') or context.endswith(' operator--'):
+      return False
 
-  # A single unnamed argument for a function tends to look like old style cast.
-  # If we see those, don't issue warnings for deprecated casts.
-  remainder = line[match.end(0):]
-  if Match(r'^\s*(?:;|const\b|throw\b|final\b|override\b|[=>{),]|->)',
-           remainder):
-    return False
+    # A single unnamed argument for a function tends to look like old style cast.
+    # If we see those, don't issue warnings for deprecated casts.
+    remainder = line[match.end(0):]
+    if Match(r'^\s*(?:;|const\b|throw\b|final\b|override\b|[=>{),]|->)',
+             remainder):
+      return False
 
-  # At this point, all that should be left is actual casts.
-  error(filename, linenum, 'readability/casting', 4,
-        'Using C-style cast.  Use %s<%s>(...) instead' %
-        (cast_type, match.group(1)))
+    # At this point, all that should be left is actual casts.
+    error(filename, linenum, 'readability/casting', 4,
+          'Using C-style cast.  Use %s<%s>(...) instead' %
+          (cast_type, match.group(1)))
 
   return True
 
@@ -6472,6 +6486,8 @@ def ProcessConfigOverrides(filename):
               SetCommentStyle(int(val));
             elif name == 'lint_blank_line':
               SetBlankLine(int(val));
+            elif name == 'lint_cstyle_cast':
+              SetCstyleCast(int(val));
           else:
             sys.stderr.write(
                 'Invalid configuration option (%s) in file %s\n' %
@@ -6705,8 +6721,8 @@ def ParseArguments(args):
       sys.stdout.write("The LINT.cfg configuration file is generated successfully.")
       sys.exit(0)
     elif opt == '--about':
-      sys.stdout.write("Version: 0.1\n")
-      sys.stdout.write("Release: 2022-02-21\n")
+      sys.stdout.write("Version: 0.3\n")
+      sys.stdout.write("Release: 2022-03-10\n")
       sys.stdout.write("Contact: skull.gu@gmail.com\n")
       sys.exit(0)
 
